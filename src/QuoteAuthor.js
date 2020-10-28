c1670768471321ab24699bb56a1e1763c06eca8a
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2020-10-28 23:47:00 +0100

    styling added

--- src/QuoteAuthor.js
+++ src/QuoteAuthor.js
@@ -1,46 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { FaQuoteRight, FaQuoteLeft, FaTwitter } from 'react-icons/fa';
 
-const QuoteAuthor = ({ displayColor, handleClick, allQuote }) => {
-    //const randomColor = displayColor();
-    //const html = document.documentElement;
-    //html.style.backgroundColor = randomColor;
-
-    const { text, author } = allQuote;
+const QuoteAuthor = ({ handleClick, quotes }) => {
+    const { text, author } = quotes;
     
     return (
-        <article style={{ backgroundColor: "white" }} className="quotebox">
-            
+        <article className="quotebox">
             <blockquote
-                //className="fadeIn"
+                className="fadeIn"
                 key={Math.random()}
-                //style={{ color: randomColor }}
             >
-                <h1>{text}</h1>
-                <h5>
-                    ~{author ? author : "Unknown"}~
+                <h1 className='quote'>
+                    <FaQuoteLeft style={{ fontSize: "14px" }} />
+                    {text}
+                    <FaQuoteRight style={{ fontSize: "14px" }} />
+                </h1>
+                <h5 className='author'>
+                    ~{author ? author : "Unknown"}
                 </h5>
             </blockquote>
             
-            <button
-                //style={{ backgroundColor: randomColor }}
-                onClick={handleClick}
-            >
-                New quote
-            </button>
+            <div className='quoteButton'>
+                <button
+                    onClick={handleClick}
+                    className='newQuote'
+                >
+                    New Quote
+                </button>
 
-            <button
-                onClick={() => {
-                    window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent(allQuote.text + '~~' + allQuote.author))
-                }}
-                type="submit"> Tweet Quote
-            </button>
-            
+                <button
+                    className='tweetQuote'
+                    onClick={() => {
+                        window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent(quotes.text + '~' + quotes.author))
+                    }}
+                    type="submit"
+                    title="Tweet Quote">
+                    <FaTwitter style={{ fontSize: '1.2rem', marginTop: "0px" }}
+                    />
+                </button>
+            </div>
         </article>
     )
 }
 
 export default QuoteAuthor
-
-// <a href={`https://twitter.com/intent/tweet?text= ${text} ~${author}`} target="_blank" title="Post this quote on twitter!" id='tweet-quote'>
-//             <i className="fab fa-twitter twitter-icon" />Share Quote
-//             </a>
